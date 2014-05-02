ece646addc97027ed415ab97c22d132db1757065
Sean Marcia <sean@cs.uoregon.edu>
2014-05-02 12:25:33 -0400

    Added routes to the appjs.

--- champion/app/js/app.js
+++ champion/app/js/app.js
@@ -3,6 +3,13 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('F1FeederApp', [
+  'F1FeederApp.services',
   'F1FeederApp.controllers',
-  'F1FeederApp.services'
-]);
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+  when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+  otherwise({redirectTo: '/drivers'});
+}]);
