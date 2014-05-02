275d2587e80beb8f9229028b6c4aae6f3f631052
Sean Marcia <sean@cs.uoregon.edu>
2014-05-02 12:16:08 -0400

    Updated our app.js to include our new service.

--- champion/app/js/app.js
+++ champion/app/js/app.js
@@ -3,5 +3,6 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('F1FeederApp', [
-  'F1FeederApp.controllers'
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
 ]);
