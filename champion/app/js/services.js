7fb0abc81af62853c4a34f463cf58fa24efb28bd
Sean Marcia <sean@cs.uoregon.edu>
2014-05-02 12:29:42 -0400

    Updated our service to pull more information about our driver.

--- champion/app/js/services.js
+++ champion/app/js/services.js
@@ -5,8 +5,8 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('F1FeederApp.services', []).
-  factory('ergastAPIservice', function($http) {
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http) {
 
     var ergastAPI = {};
 
@@ -14,6 +14,20 @@ angular.module('F1FeederApp.services', []).
       return $http({
         method: 'JSONP', 
         url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverDetails = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverRaces = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
       });
     }
 
